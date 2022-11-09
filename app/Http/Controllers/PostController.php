2d9460b823fa2fe226097f89c0e46b86a0938b95
arinkoariari <yamaguchi.kohei.47s@st.kyoto-u.ac.jp>
2022-11-09 13:08:29 +0000

    8-6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,4 +39,9 @@ public function update(PostRequest $request, Post $post)
     $post->fill($input)->save();
     return redirect('/posts/' . $post->id);
 }
+public function delete(Post $post)
+{
+    $post->delete();
+    return redirect('/');
+}
 }
